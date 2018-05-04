68e3b7d638e863da5c25a3708708407938928993
Matheus Moreira <matheus.moreira2@gmail.com>
2018-05-04 14:03:56 -0300

    Changed AssemblyInfo.cs version to 1.5.0.0

--- src/CurrencyTextBoxControl/Properties/AssemblyInfo.cs
+++ src/CurrencyTextBoxControl/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CurrencyTextBoxControl")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Wpf numeric editor with right to left digit insertion")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CurrencyTextBoxControl")]
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
